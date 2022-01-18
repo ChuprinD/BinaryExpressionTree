5601e7b31811459ca729fb2bb4bd3855bfc90522
ChuprinD <chuprin2005@yandex.ru>
2022-01-18 19:18:27 +0300

    final version

--- BinaryExpressionTree.cpp
+++ BinaryExpressionTree.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -27,14 +28,21 @@ class Node
         }
     };
 
-string delete_extreme_parentheses(string s)
+bool is_extreme_parentheses(vector<string> s)
     {
-    if (s.front() == '(' && s.back() == ')')
+    int balance = 0;
+    for (int i = 0; i < s.size() - 1; i++)
         {
-        s.erase(0, 1);
-        s.pop_back();
+        if (s[i] == "(")
+            balance++;
+        else if (s[i] == ")")
+            balance--;
+
+        if (balance == 0)
+            return false;
         }
-    return s;
+    
+    return true;
     }
 
 bool isNumber(string elem)
@@ -44,50 +52,102 @@ bool isNumber(string elem)
     return true;
     }
 
-Node* create_tree(Node* tree, string str)
-    {
+
+Node* infix_create_tree(Node* tree, vector<string> str)
+    {  
     int balance = 0;
     int min_balance = 0;
     int index = 0;
-    int size = 0;
-    str = delete_extreme_parentheses(str);
-    stringstream s;
-    s << str;
-    string cur;
-    s >> cur;
-    string elem = cur;
 
-    for (int i = 0; s.tellg() != -1; i++)
-        {  
-        if (cur == "(")
-            balance++;
-        if (cur == ")")
-            balance--;
+    if (str.size() > 1 && is_extreme_parentheses(str))
+        {
+        str.pop_back();
+        str.erase(str.begin(), str.begin() + 1);
+        }
 
-        if (!isNumber(cur) && balance <= min_balance)
+    for (int i = 0; i < str.size(); i++)
+        {
+        if (str[i] == "(") balance++;
+        else if (str[i] == ")") balance--;
+        else if (!isNumber(str[i]) && balance <= min_balance)
             {
             min_balance = balance;
-            index = s.tellg();
-            elem = cur;
+            index = i;
             }
-        s >> cur;
-        size = i;
         }
 
     if (!tree)
-        tree = new Node(elem);
+        tree = new Node(str[index]);
     else
-        tree -> elem_ = elem;
+        tree->elem_ = str[index];
 
-    string first = s.str().substr(0, index - 1);
-    string second = s.str().substr(index + 1, s.str().size() - index);
-
-    if (size != 0)
+    if (index != 0)
         {
-        tree->left_ = create_tree(tree->left_, first);
-        tree->right_ = create_tree(tree->right_, second);
+        vector <string> first = {};
+        balance = 0;
+        for (int i = index - 1; i >= 0; i--)
+            {
+            if (str[i] == "(") balance++;
+            if (str[i] == ")") balance--;
+
+            if (str[i] == "(" && balance > 0)
+                continue;
+
+            first.push_back(str[i]);
+            }
+        reverse(first.begin(), first.end());
+
+        vector <string> second = {};
+        balance = 0;
+        for (int i = index + 1; i < str.size(); i++)
+            {
+            if (str[i] == "(") balance++;
+            if (str[i] == ")") balance--;
+
+            if (str[i] == ")" && balance <= 0)
+                continue;
+
+            second.push_back(str[i]);
+            }
+
+        tree->left_ = infix_create_tree(tree->left_, first);
+        tree->right_ = infix_create_tree(tree->right_, second);
         }
 
+    return tree;
+    }
+
+Node* postfix_create_tree(Node* tree, vector<string>& str)
+    {
+    if (!tree)
+        tree = new Node(str[str.size() - 1]);
+    else
+        tree->elem_ = str[str.size() - 1];
+
+    str.pop_back();
+
+    if (isNumber(tree->elem_)) return tree;
+
+    tree->right_ = postfix_create_tree(tree->right_, str);
+    tree->left_ = postfix_create_tree(tree->left_, str);
+
+    return tree;
+    }
+
+Node* prefix_create_tree(Node* tree, vector<string>& str)
+    {
+    if (!tree)
+        tree = new Node(str[0]);
+    else
+        tree->elem_ = str[0];
+
+    str.erase(str.begin());
+
+    if (isNumber(tree->elem_)) return tree;
+
+    tree->left_ = prefix_create_tree(tree->left_, str);
+    tree->right_ = prefix_create_tree(tree->right_, str);
+  
     return tree;
     }
 
@@ -102,7 +162,41 @@ void print(Node* tree, int level)
         }
     }
 
-int main()
+void print_infix(Node* tree)
+    {
+    if (tree)
+        {
+        if (!isNumber(tree->elem_))
+            cout << "(";
+        print_infix(tree->left_);
+        cout << tree->elem_;
+        print_infix(tree->right_);
+        if (!isNumber(tree->elem_))
+            cout << ")";
+        }
+    }
+
+void print_postfix(Node* tree)
+    {
+    if (tree)
+        {
+        print_postfix(tree->left_);
+        print_postfix(tree->right_);
+        cout << tree->elem_ << " ";
+        }
+    }
+
+void print_prefix(Node* tree)
+    {
+    if (tree)
+        {
+        cout << tree->elem_ << " ";
+        print_prefix(tree->left_);
+        print_prefix(tree->right_);
+        }
+    }
+
+vector <string> get_string()
     {
     string sin;
     getline(cin, sin);
@@ -113,27 +207,68 @@ int main()
         {
         string tmp = "";
         if (sin[i] != ' ')
-        {
+            {
             if (!isdigit(sin[i]))
                 tmp += sin[i];
             else
-            {
+                {
                 while (isdigit(sin[i]))
                     tmp += sin[i++];
-                //i--;
+                i--;
+                }
             }
-        }
-        s.push_back(tmp);
+        if (tmp != "")
+            s.push_back(tmp);
         i++;
-            
         }
 
-    for (string str : s)
-        cout << str;
+    return s;
+    }
 
-    /*Node tree;
-    create_tree(&tree, sin);
-    print(&tree, 0);*/
+int main()
+    {
+    Node tree;
+    string type;
+    getline(cin, type);
+    vector <string> s = get_string();
+    
+    if (type == "prefix")
+        {
+        prefix_create_tree(&tree, s);
+        
+        cout << "infix: ";
+        print_infix(&tree);
+        cout << endl;
 
+        cout << "postfix: ";
+        print_postfix(&tree);
+        cout << endl;
+        }
+    else if (type == "postfix")
+        {
+        postfix_create_tree(&tree, s);
+
+        cout << "infix: ";
+        print_infix(&tree);
+        cout << endl;
+
+        cout << "prefix: ";
+        print_prefix(&tree);
+        cout << endl;
+        }
+    else
+        {
+        infix_create_tree(&tree, s);
+
+        cout << "postfix: ";
+        print_postfix(&tree);
+        cout << endl;
+
+        cout << "prefix: ";
+        print_prefix(&tree);
+        cout << endl;
+        }
+
+    system("pause");
     return 0;
     }
